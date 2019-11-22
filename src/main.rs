a2572e6cf0b472831f367d5b3cf7db06bab39c05
Jiajie Chen <noc@jiegec.ac.cn>
2019-11-22 21:06:03 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use colored::*;
+use reqwest;
+use serde::Deserialize;
+use std::time::SystemTime;
+
+#[derive(Deserialize, Debug)]
+struct TunasyncStatus {
+    name: String,
+    is_master: bool,
+    status: String,
+    last_update: String,
+    last_update_ts: i64,
+    last_ended: String,
+    last_ended_ts: i64,
+    upstream: String,
+    size: String,
+}
+
+fn main() {
+    for server in ["neomirrors", "nanomirrors"].iter() {
+        let client = reqwest::Client::new();
+        let mut res = client
+            .get(&format!(
+                "https://{}.tuna.tsinghua.edu.cn/static/tunasync.json",
+                server
+            ))
+            .header(reqwest::header::USER_AGENT, "tunasync-monitor")
+            .send()
+            .unwrap();
+        let mut status: Vec<TunasyncStatus> = res.json().unwrap();
+        status.sort_by_key(|status| -status.last_update_ts);
+        let time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+        let mut fail = false;
+        for entry in status {
+            if entry.status == "failed" {
+                let expired = time - entry.last_update_ts;
+                if expired > 60 * 60 * 24 * 7 && entry.last_update_ts > 0 {
+                    // one week
+                    println!(
+                        "{} {}: {}, {} days ago",
+                        server.blue(),
+                        "failed".red(),
+                        entry.name,
+                        (time - entry.last_update_ts) / (60 * 60 * 24),
+                    );
+                }
+                fail = true;
+            }
+        }
+
+        if !fail {
+            println!("{} {}: *", server.blue(), "success".red());
+        }
+    }
+}
